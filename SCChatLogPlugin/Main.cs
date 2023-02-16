a1ac8d2515f9667e51f2aacaf2a72baafac588bb
sunbeam9 <61368572+sunbeam9@users.noreply.github.com>
2023-02-16 15:10:23 -0500

    First Commit

---
+++ SCChatLogPlugin/Main.cs
@@ -0,0 +1,48 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace SCChatLogPlugin
+{
+    public class SCChatLogPlugin : RocketPlugin<SCChatLogPluginConfiguration>
+    {
+        protected override void Load()
+        {
+            Logger.Log(Configuration.Instance.LoadMessage);
+            ChatManager.onChatted += OnPlayerChat;
+        }
+
+        public static void sendDiscordWebhook(string url, string json)
+        {
+            var request = WebRequest.Create(url);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                streamWriter.Write(json);
+            request.GetResponse();
+        }
+
+        private void OnPlayerChat(SteamPlayer player, EChatMode mode, ref Color chatted, ref bool isRich, string text, ref bool isVisible)
+        {
+            var uPlayer = UnturnedPlayer.FromSteamPlayer(player);
+            var webhook = Configuration.Instance.DiscordWebhook;
+            var name = uPlayer.DisplayName;
+            var profile = uPlayer.SteamProfile.AvatarIcon;
+
+            sendDiscordWebhook(webhook, ($"{{ \"embeds\": [{{\"description\": \"{text}\", \"color\": null, \"author\": {{\"name\": \"{name}\", \"icon_url\": \"{profile}\"}}}}], \"attachments\": []}}"));
+        }
+        protected override void Unload()
+        {
+            Logger.Log(Configuration.Instance.UnloadMessage);
+        }
+    }
+}
---
+++ SCChatLogPlugin/SCChatLogPluginConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCChatLogPlugin
+{
+    public class SCChatLogPluginConfiguration : IRocketPluginConfiguration
+    {
+
+        public string LoadMessage { get; set; }
+        public string UnloadMessage { get; set; }
+        
+        public string DiscordWebhook { get; set; }
+        public void LoadDefaults()
+        {
+            LoadMessage = $"\n##############################\n SC Chat Log Plugin has loaded.\n##############################\n";
+            UnloadMessage = $"\n##############################\n SC Chat Log Plugin has unloaded.\n##############################\n";
+            DiscordWebhook = "https://discord.com/api/webhooks/1075656014847823913/g5Ucx0WywDvnLOYmgJqkKiwNICquTQy1lXnn_8ozitIgya3_p5dYR_Twx2ND_M0phEjp";
+        }
+    }
+}
